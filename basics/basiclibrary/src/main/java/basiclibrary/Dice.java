1721ae108b397a632c1d24cc18220b912432ae7b
Chandler Puckett <chandler.puckett1@gmail.com>
2020-09-15 17:37:32 -0700

    dice roll, duplicate method

---
+++ basics/basiclibrary/src/main/java/basiclibrary/Dice.java
@@ -0,0 +1,40 @@
+package basiclibrary;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Dice {
+
+    private int dice;
+
+    private int[] roll (int n) {
+//      while loop to roll
+        int count = 0;
+        int[] rollValues = new int[n];
+
+        while (count < n){
+//      Roll Dice here
+            rollValues[count] = (int) (Math.random()*6) + 1;
+            count++;
+        }
+        return rollValues;
+    }
+
+    private boolean containsDuplicates (int[] arr){
+
+        boolean test = false;
+
+//      Hash Set stuff:
+//      https://javarevisited.blogspot.com/2015/06/3-ways-to-find-duplicate-elements-in-array-java.html
+
+        Set<Integer> store = new HashSet<>();
+
+        for (int val : arr){
+            if (!store.add(val)){
+                test = true;
+            }
+        }
+        return test;
+    }
+
+}
